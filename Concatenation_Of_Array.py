233b87e9efb247f67b828f133a49f3be608d726f
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-17 21:38:47 +0530

    Add files via upload

---
+++ Concatenation_Of_Array.py
@@ -0,0 +1,11 @@
+"""
+Link of the question : https://leetcode.com/problems/concatenation-of-array/
+Leetcode question number : 1929
+"""
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return 2*nums
