f9d8305dbd4678a3fefa9b1935d0fe377c41d70e
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-20 10:16:26 +0530

    Add files via upload

---
+++ Pow(x,n).cpp
@@ -0,0 +1,12 @@
+/*
+Link of the question : https://leetcode.com/problems/powx-n/
+Leetcode question number : 50
+*/
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        return pow(x, n);
+    }
+};
