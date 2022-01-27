74e975754d6db6cfa9b82bce339d0e8edd7d60bf
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-27 12:20:54 +0530

    Add files via upload

---
+++ Smallest_Index_With_Equal_Value_2.cpp
@@ -0,0 +1,21 @@
+/*
+Link of the question : https://leetcode.com/problems/smallest-index-with-equal-value/
+Leetcode question number : 2057
+*/
+class Solution
+{
+public:
+    int smallestEqual(vector<int> &nums)
+    {
+        int i;
+        for (i = 0; i < nums.size(); i++)
+        {
+            if (i % 10 == nums[i])
+            {
+                return i;
+                break;
+            }
+        }
+        return -1;
+    }
+};
