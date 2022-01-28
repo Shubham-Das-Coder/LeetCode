55d5e0fd7a1a82fb0cb9f647f8082e5a7d1d8244
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-28 19:59:26 +0530

    Add files via upload

---
+++ Maximum_Subarray_1.cpp
@@ -0,0 +1,20 @@
+/*
+Link of the question : https://leetcode.com/problems/maximum-subarray/
+Leetcode question number : 53
+*/
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int ans = INT_MIN, sum = 0;
+        for (auto a : nums)
+        {
+            sum += a;
+            ans = max(ans, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+        return ans;
+    }
+};
