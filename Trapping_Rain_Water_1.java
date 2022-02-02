967b38b6cba2c4882229edc7bfc288f40a5ba212
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-02 19:53:26 +0530

    Add files via upload

---
+++ Trapping_Rain_Water_1.java
@@ -0,0 +1,31 @@
+/*
+Link of the question : https://leetcode.com/problems/trapping-rain-water/
+Leetcode question number : 42
+*/
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int left=0,right=n-1,res=0;
+        int maxleft=0,maxright=0;
+        while(left<=right)
+        {
+            if(height[left]<=height[right])
+            {
+                if(height[left]>=maxleft)
+                    maxleft=height[left];
+                else
+                    res+=maxleft-height[left];
+                left++;
+            }
+            else
+            {
+                if(height[right]>=maxright)
+                    maxright=height[right];
+                else
+                    res+=maxright-height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
