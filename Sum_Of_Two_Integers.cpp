935288d36ed5d1ab16d42e68d3b754edc014e7c3
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-07 11:32:07 +0530

    Add files via upload

---
+++ Sum_Of_Two_Integers.cpp
@@ -0,0 +1,12 @@
+/*
+Link of the question : https://leetcode.com/problems/sum-of-two-integers/
+Leetcode question number : 371
+*/
+class Solution
+{
+public:
+    int getSum(int a, int b)
+    {
+        return a + b;
+    }
+};
