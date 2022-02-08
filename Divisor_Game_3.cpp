1e9efc712ef70388df7f53e2e125a6ad0bff3b00
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-08 16:35:38 +0530

    Add files via upload

---
+++ Divisor_Game_3.cpp
@@ -0,0 +1,12 @@
+/*
+Link of the question : https://leetcode.com/problems/divisor-game/
+Leetcode question number : 1025
+*/
+class Solution
+{
+public:
+    bool divisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+};
