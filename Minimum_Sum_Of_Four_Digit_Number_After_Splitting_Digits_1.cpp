1bfa1db72b159abe509e3d36214d1eaa81095050
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-11 20:13:57 +0530

    Add files via upload

---
+++ Minimum_Sum_Of_Four_Digit_Number_After_Splitting_Digits_1.cpp
@@ -0,0 +1,15 @@
+/*
+Link of the question : https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+Leetcode question number : 2160
+*/
+class Solution
+{
+public:
+    int minimumSum(int num)
+    {
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        int res = (s[0] - '0' + s[1] - '0') * 10 + s[2] - '0' + s[3] - '0';
+        return res;
+    }
+};
