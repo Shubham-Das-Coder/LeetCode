260077cd42bacb07760328990aeb1f9194c13f65
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-13 22:51:56 +0530

    Add files via upload

---
+++ Count_Operations_To_Obtain_Zero_1.cpp
@@ -0,0 +1,21 @@
+/*
+Link of the question : https://leetcode.com/problems/count-operations-to-obtain-zero/
+Leetcode question number : 2169
+*/
+class Solution
+{
+public:
+    int countOperations(int num1, int num2)
+    {
+        int cnt = 0;
+        while (num1 && num2)
+        {
+            if (num1 >= num2)
+                num1 -= num2;
+            else
+                num2 -= num1;
+            cnt++;
+        }
+        return cnt;
+    }
+};
