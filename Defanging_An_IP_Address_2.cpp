036148e63d6a48652bddd7f21e64ae870d3c4860
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-17 13:50:44 +0530

    Add files via upload

---
+++ Defanging_An_IP_Address_2.cpp
@@ -0,0 +1,22 @@
+/*
+Link of the question : https://leetcode.com/problems/defanging-an-ip-address/
+Leetcode question number : 1108
+*/
+class Solution
+{
+public:
+    string defangIPaddr(string address)
+    {
+        string res;
+        for (auto c : address)
+        {
+            if (c == '.')
+            {
+                res += "[.]";
+            }
+            else
+                res += c;
+        }
+        return res;
+    }
+};
