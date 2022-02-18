cf3f3a5638a52d97582049dfde144a858087cecd
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-18 22:01:39 +0530

    Add files via upload

---
+++ Number_Of_Students_Doing_Homework_At_A_Given_Time_1.cpp
@@ -0,0 +1,18 @@
+/*
+Link of the question : https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+Leetcode question number : 1450
+*/
+class Solution
+{
+public:
+    int busyStudent(vector<int> &startTime, vector<int> &endTime, int queryTime)
+    {
+        int cnt = 0;
+        for (int i = 0; i < startTime.size(); i++)
+        {
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+                cnt++;
+        }
+        return cnt;
+    }
+};
