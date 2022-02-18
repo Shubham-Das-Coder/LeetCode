068ccf8bdf2f605f7beb35911a4a6a461a3a441c
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-18 16:11:57 +0530

    Add files via upload

---
+++ Find_Target_Indices_After_Sorting_Array_1.cpp
@@ -0,0 +1,19 @@
+/*
+Link of the question : https://leetcode.com/problems/find-target-indices-after-sorting-array/
+Leetcode question number : 2089
+*/
+class Solution
+{
+public:
+    vector<int> targetIndices(vector<int> &nums, int target)
+    {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == target)
+                ans.insert(ans.end(), i);
+        }
+        return ans;
+    }
+};
