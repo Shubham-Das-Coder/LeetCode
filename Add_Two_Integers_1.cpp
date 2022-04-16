c56bb9649e001ce6a2998269ddec6e51c0abf8b2
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-04-16 18:58:24 +0530

    Add files via upload

---
+++ Add_Two_Integers_1.cpp
@@ -0,0 +1,12 @@
+/*
+Link of the question : https://leetcode.com/problems/add-two-integers/
+Leetcode question number : 2235
+*/
+class Solution
+{
+public:
+    int sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+};
