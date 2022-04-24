9b39126966d9a3d526a80c2becaddc5e92981e0f
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-04-24 18:06:08 +0530

    Add files via upload

---
+++ Remove_Duplicates_From_Sorted_List_II_1.cpp
@@ -0,0 +1,37 @@
+/*
+Link of the question : https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+Leetcode question number : 82
+*/
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        ListNode *p = new ListNode(-1);
+        p->next = head;
+        ListNode *head1 = p;
+        while (p->next != NULL && p->next->next != NULL)
+        {
+            ListNode *p1 = p->next;
+            ListNode *p2 = p->next->next;
+            if (p1->val == p2->val)
+            {
+                ListNode *temp = p2;
+                while (temp != NULL && p1->val == temp->val)
+                {
+                    temp = temp->next;
+                }
+                p->next = temp;
+            }
+            else
+            {
+                p = p->next;
+            }
+        }
+        return head1->next;
+    }
+};
