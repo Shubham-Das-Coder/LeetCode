5e02b2dce619c3b884824ff40094b7f5d338ef03
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-05-26 07:21:45 +0530

    Add files via upload

---
+++ Number_Of_1_Bits_1.cpp
@@ -0,0 +1,19 @@
+/*
+Link of the question : https://leetcode.com/problems/number-of-1-bits/
+Leetcode question number : 191
+*/
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int cnt = 0;
+        while (n > 0)
+        {
+            if (n % 2 == 1)
+                cnt++;
+            n /= 2;
+        }
+        return cnt;
+    }
+};
