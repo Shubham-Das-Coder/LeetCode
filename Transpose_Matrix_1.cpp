faaaafb0b2e0136046bbb3bae623fb1820325f00
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-06-02 15:15:42 +0530

    Add files via upload

---
+++ Transpose_Matrix_1.cpp
@@ -0,0 +1,22 @@
+/*
+Link of the question : https://leetcode.com/problems/transpose-matrix/
+Leetcode question number : 867
+*/
+class Solution
+{
+public:
+    vector<vector<int>> transpose(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+};
