80ad3b4c97ee189b610befa3fe3f6f83a496d323
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-06-08 21:18:14 +0530

    Add files via upload

---
+++ Find_Triangular_Sum_Of_An_Array_2.cpp
@@ -0,0 +1,18 @@
+/*
+Link of the question : https://leetcode.com/problems/find-triangular-sum-of-an-array/
+Leetcode question number : 2221
+*/
+class Solution
+{
+public:
+    int triangularSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        while (n--)
+        {
+            for (int i = 0; i < n; ++i)
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+        return nums[0];
+    }
+};
