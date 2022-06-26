ffde7b9485018bc5e48e53e3c5a05680337ef103
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-06-26 14:13:18 +0530

    Add files via upload

---
+++ Non-Decreasing_Array_1.cpp
@@ -0,0 +1,26 @@
+/*
+Link of the question : https://leetcode.com/problems/non-decreasing-array/
+Leetcode question number : 665
+*/
+class Solution
+{
+public:
+    bool checkPossibility(vector<int> &nums)
+    {
+        int cnt = 0;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] < nums[i - 1])
+            {
+                cnt++;
+                if (cnt > 1)
+                    return false;
+                if (i == 1 || nums[i - 2] <= nums[i])
+                    nums[i - 1] = nums[i];
+                else
+                    nums[i] = nums[i - 1];
+            }
+        }
+        return true;
+    }
+};
