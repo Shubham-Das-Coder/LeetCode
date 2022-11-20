3f613b096ccae11cd7d5ab93b5ef25b295922fc8
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-11-20 19:06:26 +0530

    Add files via upload

---
+++ Ugly_Number_1.py
@@ -0,0 +1,12 @@
+'''
+Link of the question : https://leetcode.com/problems/ugly-number/
+Leetcode question number : 263
+'''
+
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        for x in [2, 3, 5]:
+            while n > 1 and n % x == 0:
+                n //= x
+        return n == 1
