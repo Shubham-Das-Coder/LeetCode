74dff0eea689aba71f42e51c196a06a2a61cdde0
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2023-01-03 12:10:21 +0530

    Add files via upload

---
+++ Delete_Columns_To_Make_Sorted_1.cpp
@@ -0,0 +1,24 @@
+/*
+Link of the question : https://leetcode.com/problems/delete-columns-to-make-sorted/
+Leetcode question number : 944
+*/
+class Solution
+{
+public:
+    int minDeletionSize(vector<string> &strs)
+    {
+        int ans = 0;
+        for (int j = 0; j < strs[0].size(); j++)
+        {
+            for (int i = 1; i < strs.size(); i++)
+            {
+                if (strs[i - 1][j] > strs[i][j])
+                {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
