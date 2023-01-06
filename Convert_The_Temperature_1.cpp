d07f888aede846b6aa8136c2371ebd6c795894eb
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2023-01-06 22:00:26 +0530

    Add files via upload

---
+++ Convert_The_Temperature_1.cpp
@@ -0,0 +1,15 @@
+/*
+Link of the question : https://leetcode.com/problems/convert-the-temperature/
+Leetcode question number : 2469
+*/
+class Solution
+{
+public:
+    vector<double> convertTemperature(double celsius)
+    {
+        vector<double> ans;
+        ans.push_back(celsius + 273.15);
+        ans.push_back(celsius * 1.80 + 32);
+        return ans;
+    }
+};
